443f2f1b2fb08643c50e423bf1900e8b32143818
yancai <yancai915@gmail.com>
2013-12-26 09:07:19 +0800

    create

--- Demos/threading_demo.py
+++ Demos/threading_demo.py
@@ -2,6 +2,44 @@
 # -*- coding:utf-8 -*-
 # Filename: 'threading_demo.py'
 # Author:   'yancai'
-# Date:     '13-12-26'
 
 """Documentation"""
+
+
+import threading
+import time
+
+
+FRUITS = [
+    "banana",
+    "pear",
+    "cherry"
+]
+
+
+def kid(fruit, n=1):
+    for i in range(n):
+        print("I want " + fruit)
+        time.sleep(0.5)
+
+
+def main():
+    threads = []
+    times_ = 3
+    for i in range(times_):
+        threads.append(threading.Thread(
+            target=kid,
+            args=(FRUITS[i], 10)
+        ))
+
+    for i in range(times_):
+        threads[i].start()
+
+    for i in range(times_):
+        threads[i].join()
+
+    raw_input("all done")
+
+if __name__ == "__main__":
+    main()
+    pass
